d22f90709aff359736184ef17e179b10a5c020a9
JakubicekRoman <jakubicek@vutbr.cz>
2022-01-20 14:56:33 +0000

    new dataset, resaving, preparation for training

--- StT_data_valid.py
+++ StT_data_valid.py
@@ -13,8 +13,9 @@ import random
 import torchvision.transforms as T
 import pandas as pd
 import cv2
+import pickle
 
-
+import Utilities as Util
 import Loaders
 import Unet_2D
 
@@ -27,21 +28,22 @@ def CreateDataset7(path_data):
         f = os.listdir(os.path.join(path_data, pat_name))
         for _,file in enumerate(f):
             if file.find('_gt')>0:
-                path_mask = os.path.join(path_data, pat_name, file)
-                name = file[0:file.find('_gt')]
-                path_maps = os.path.join(path_data, pat_name, name+'.nii.gz')
-                
-                sizeData = Loaders.size_nii( path_maps )
-                if len(sizeData)==2:
-                    sizeData = sizeData + (1,)
-                print(sizeData)
-                
-                for sl in range(0,sizeData[2]):
-                    data_list_tr.append( {'img_path': path_maps,
-                                          'mask_path': path_mask,
-                                          'pat_name': pat_name,
-                                          'file_name': name,
-                                          'Slice': sl } )
+                # if file.find('Joint')>=0:
+                    path_mask = os.path.join(path_data, pat_name, file)
+                    name = file[0:file.find('_gt')]
+                    path_maps = os.path.join(path_data, pat_name, name+'.nii.gz')
+                    
+                    sizeData = Loaders.size_nii( path_maps )
+                    if len(sizeData)==2:
+                        sizeData = sizeData + (1,)
+                    # print(sizeData)
+                    
+                    for sl in range(0,sizeData[2]):
+                        data_list_tr.append( {'img_path': path_maps,
+                                              'mask_path': path_mask,
+                                              'pat_name': pat_name,
+                                              'file_name': name,
+                                              'slice': sl } )
             
     return data_list_tr
 
@@ -49,8 +51,91 @@ def CreateDataset7(path_data):
 ## -------------- validation for \StT data annotated ------------------
 path_data = '/data/rj21/Data/Data2/Resaved_data_StT'  # Linux bioeng358
 # path_data = 'D:\jakubicek\SegmMyo\Data_ACDC\\training'  # Win CUDA2
-data_list_train = CreateDataset7(os.path.normpath( path_data ))
+data_list_test = CreateDataset7(os.path.normpath( path_data ))
+
+file_name = "data_list_Data2_all.pkl"
+open_file = open(file_name, "wb")
+pickle.dump(data_list_test, open_file)
+open_file.close()
+# open_file = open(file_name, "rb")
+# data_list_test = pickle.load(open_file)
+# open_file.close()
 
 
+net = torch.load(r"/data/rj21/MyoSeg/Models/net_v1_5.pt")
+net = net.cuda()
 
+batch = 1
 
+diceTe=[];
+
+for num in range(0,len(data_list_test),5):
+# for num in range(22,23,1):    
+   
+    t=0
+    Imgs = torch.tensor(np.zeros((batch,1,128,128) ), dtype=torch.float32)
+    Masks = torch.tensor(np.zeros((batch,2,128,128) ), dtype=torch.float32)
+
+    
+    for b in range(0,batch):
+        current_index = data_list_test[num+b]['slice']
+        img_path = data_list_test[num+b]['img_path']
+        mask_path = data_list_test[num+b]['mask_path']
+        nPat = data_list_test[num+b]['pat_name']
+    
+        img = Loaders.read_nii( img_path, (0,0,current_index,t) )
+        mask = Loaders.read_nii( mask_path, (0,0,current_index,t) )
+        
+        img = Util.crop_center(img, new_width=128, new_height=128)
+        mask = Util.crop_center(mask, new_width=128, new_height=128)
+        
+        # print(np.shape(img))
+        
+        # Fig = plt.figure()
+        # plt.imshow(img, cmap='gray')
+        # plt.imshow(mask*0.2, cmap='cool', alpha=0.2)
+        # plt.show()
+        
+        img = np.expand_dims(img, 0).astype(np.float32)
+        Imgs[b,0,:,:] = torch.tensor(img)
+        
+        mask = np.expand_dims(mask, 0).astype(np.float32)
+        Masks[b,0,:,:] = torch.tensor(mask)
+    
+    
+    with torch.no_grad(): 
+        res = net( Imgs.cuda() )
+        res = torch.softmax(res,dim=1)                    
+    
+    torch.cuda.empty_cache()
+    
+    dice = Util.dice_coef( res[:,0,:,:]>0.5, Masks[:,0,:,:].cuda() )                
+    diceTe.append(dice.detach().cpu().numpy())    
+
+    resB = (res[0,0,:,:].detach().cpu().numpy()>0.5).astype(np.dtype('uint8'))
+    dimg = cv2.dilate(resB, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3)) )
+    ctr = dimg - resB
+    
+    GT = (Masks[0,0,:,:].detach().cpu().numpy()>0.5).astype(np.dtype('uint8'))
+    dimg = cv2.dilate(GT, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3)) )
+    ctrGT = dimg - GT
+    
+    imgB = Imgs[0,0,:,:].detach().numpy()
+    imgB = ( imgB - imgB.min() ) / (imgB.max() - imgB.min())
+    RGB_imgB = cv2.cvtColor(imgB,cv2.COLOR_GRAY2RGB)
+    
+    comp = RGB_imgB[:,:,0]
+    comp[ctr==1]=1
+    RGB_imgB[:,:,0] = comp
+    
+    comp = RGB_imgB[:,:,1]
+    comp[ctrGT==1]=1
+    RGB_imgB[:,:,1] = comp
+    
+    Fig = plt.figure()
+    plt.imshow(RGB_imgB)
+    
+    plt.show()
+    plt.draw()
+    
+print(np.mean(diceTe))
--- resaving.m
+++ resaving.m
@@ -2,74 +2,104 @@ clear all
 close all
 clc
 
-path_data = ['/data/rj21/Data/Data2/orig'];
+path_data = ['C:\Users\NB-UBMI-786\Desktop\ForRoman_20220114\Database_St' ...
+             'Thomas_annotated\Segmentated_jointT1T2'];
 
-path_save = ['/data/rj21/Data/Data2/Resaved'];
+path_save = ['C:\Users\NB-UBMI-786\Desktop\ForRoman_20220114\'...
+             'Database_StThomas_annotated\Resaved_data_StT'];
 
-D = dir([path_data , '/20*']);
+D = dir([path_data , '\20*']);
 % D(1:2)=[];
 
-for i = 1:length(D)
-%     try
-    path_save_file = [path_save '/' D(i).name];
+for i = [1:30, 32:34] %length(D)
+    %     try
+    path_save_file = [path_save '\' D(i).name];
     mkdir(path_save_file)
 
-    vel = [128,128,100];
+    % my special crop
+    vel = [128,128];
     
-    file1 = dir([D(i).folder,'/', D(i).name '/*Reference*.mat']);
-    file2 = ls([D(i).folder,'/', D(i).name '/*RefMaps_T1post.mat']);
+
+    %% for Reference data T1 and T2
+    file1 = ls([D(i).folder,'\', D(i).name '\*Reference*.mat']);
+    file2 = ls([D(i).folder,'\', D(i).name '\*RefMaps_T1post.mat']);
     if ~isempty(file2)
-        load([D(i).folder,'/', D(i).name '/' file1]);
-        niftiwrite(ReferenceMaps.T1map_pre ,[path_save_file '/'  '/Ref_T1post.nii'],'Compressed',true); 
-        load([D(i).folder,'/', D(i).name '/' file2],'MyoMask');
-        niftiwrite(MyoMask ,[path_save_file '/'  '/Ref_T1post_gt.nii'],'Compressed',true); 
+        load([D(i).folder,'\', D(i).name '\' file1]);
+        load([D(i).folder,'\', D(i).name '\' file2],'MyoMask','sl_range', 'Upsampling_factor');
+        T = ReferenceMaps.T1map_post(:,:,sl_range);
+        MyoMask = imresize(MyoMask, 1/Upsampling_factor,'nearest');
+        vel1 = max(vel,size(MyoMask,[1:2])); 
+        MyoMask = insertMatrix(zeros(vel1(1),vel1(2),size(T,3)), MyoMask);
+        T = insertMatrix(zeros(vel1(1),vel1(2),size(T,3)), T);
+        niftiwrite(T ,[path_save_file '\'  '\Ref_T1post.nii'],'Compressed',true); 
+        niftiwrite(MyoMask ,[path_save_file '\'  '\Ref_T1post_gt.nii'],'Compressed',true); 
     end
 
-    file2 = ls([D(i).folder,'/', D(i).name '/*RefMaps_T1pre.mat']);
+    file2 = ls([D(i).folder,'\', D(i).name '\*RefMaps_T1pre.mat']);
     if ~isempty(file2)
-        load([D(i).folder,'/', D(i).name '/' file1]);
-        niftiwrite(ReferenceMaps.T1map_pre ,[path_save_file '/'  '/Ref_T1pre.nii'],'Compressed',true); 
-        load([D(i).folder,'/', D(i).name '/' file2],'MyoMask');
-        niftiwrite(MyoMask ,[path_save_file '/'  '/Ref_T1pre_gt.nii'],'Compressed',true); 
+        load([D(i).folder,'\', D(i).name '\' file1]);
+        load([D(i).folder,'\', D(i).name '\' file2],'MyoMask','sl_range','Upsampling_factor');
+        T = ReferenceMaps.T1map_pre(:,:,sl_range);
+        MyoMask = imresize(MyoMask, 1/Upsampling_factor,'nearest');
+        vel1 = max(vel,size(MyoMask,[1:2])); 
+        MyoMask = insertMatrix(zeros(vel1(1),vel1(2),size(T,3)), MyoMask);
+        T = insertMatrix(zeros(vel1(1),vel1(2),size(T,3)), T);
+        niftiwrite(T ,[path_save_file '\'  '\Ref_T1pre.nii'],'Compressed',true); 
+        niftiwrite(MyoMask ,[path_save_file '\'  '\Ref_T1pre_gt.nii'],'Compressed',true); 
     end
 
-    file2 = ls([D(i).folder,'/', D(i).name '/*RefMaps_T2.mat']);
+    file2 = ls([D(i).folder,'\', D(i).name '\*RefMaps_T2.mat']);
     if ~isempty(file2)
-        load([D(i).folder,'/', D(i).name '/' file1]);
-        niftiwrite(ReferenceMaps.T1map_pre ,[path_save_file '/'  '/ef_T2.nii'],'Compressed',true); 
-        load([D(i).folder,'/', D(i).name '/' file2],'MyoMask');
-        niftiwrite(MyoMask ,[path_save_file '/'  '/Ref_T2_gt.nii'],'Compressed',true); 
+        load([D(i).folder,'\', D(i).name '\' file1]);
+        load([D(i).folder,'\', D(i).name '\' file2],'MyoMask','sl_range','Upsampling_factor');
+        T = ReferenceMaps.T2map(:,:,sl_range);
+        MyoMask = imresize(MyoMask, 1/Upsampling_factor,'nearest');
+        vel1 = max(vel,size(MyoMask,[1:2])); 
+        MyoMask = insertMatrix(zeros(vel1(1),vel1(2),size(T,3)), MyoMask);
+        T = insertMatrix(zeros(vel1(1),vel1(2),size(T,3)), T);      
+        niftiwrite(T,[path_save_file '\'  '\Ref_T2.nii'],'Compressed',true); 
+        niftiwrite(MyoMask ,[path_save_file '\'  '\Ref_T2_gt.nii'],'Compressed',true);
     end
 
-    file1 = ls([D(i).folder,'/', D(i).name '/*Segmentation.mat']);
-    file2 = ls([D(i).folder,'/', D(i).name '/*JointT1T2maps_SA.mat']);
+    %% for Joint T1/T2 data
+    file1 = ls([D(i).folder,'\', D(i).name '\*Segmentation.mat']);
+    file2 = ls([D(i).folder,'\', D(i).name '\*JointT1T2maps_SA.mat']);
 
-    load([D(i).folder,'/', D(i).name '/' file1],'MyoMask');
-    load([D(i).folder,'/', D(i).name '/' file2],'T1_SA');
-    load([D(i).folder,'/', D(i).name '/' file2],'T2_SA');
+    load([D(i).folder,'\', D(i).name '\' file1],'MyoMask','sl_range','Upsampling_factor');
+    load([D(i).folder,'\', D(i).name '\' file2],'T1_SA');
+    load([D(i).folder,'\', D(i).name '\' file2],'T2_SA');
 
-    fileInd =  ls([D(i).folder,'/', D(i).name '/Crop_Indices.mat']);
-    load([D(i).folder,'/', D(i).name '/' fileInd]);
+    fileInd =  ls([D(i).folder,'\', D(i).name '\Crop_Indices.mat']);
+    load([D(i).folder,'\', D(i).name '\' fileInd]);
 
+    MyoMask = imresize(MyoMask, 1/Upsampling_factor ,'nearest');
+%     MyoMask = imresize(MyoMask, [length(xx),length(yy)],'nearest');
     
-    Joint_mask = zeros([size(JointT2,1),size(JointT2,2),length(zz)]);
-    MyoMask = imresize(MyoMask, [length(xx),length(yy)],'nearest');
+    Joint_mask = zeros([size(T2_SA,1),size(T2_SA,2),length(zz)]);
     Joint_mask(xx,yy,:) = MyoMask;
+
     
+    %% cropping
     T2_SA = T2_SA(:,:,zz);
     T1_SA = T1_SA(:,:,zz);
-    vel1 = min(vel,size(T2_SA)); vel1(3)=length(ZZ);
-    win = centerCropWindow3d(size(T2_SA),vel1);
-    
-    JointT2 = imcrop3(T2_SA,win);
-    JointT1 = imcrop3(T1_SA,win);
-    Joint_mask = imcrop3(Joint_mask,win);
-    
-    niftiwrite(JointT1 ,[path_save_file '/'  '/Joint_T1.nii'],'Compressed',true); 
-    niftiwrite(JointT2 ,[path_save_file '/'  '/Joint_T2.nii'],'Compressed',true); 
 
-    niftiwrite(Joint_mask ,[path_save_file '/'  '/Joint_T1_gt.nii'],'Compressed',true); 
-    niftiwrite(Joint_mask ,[path_save_file '/'  '/Joint_T2_gt.nii'],'Compressed',true); 
+%     vel1 = min(vel,size(T2_SA)); vel1(3)=length(zz);
+%     win = centerCropWindow3d(size(T2_SA),vel1);
+%     JointT2 = imcrop3(T2_SA,win);
+%     JointT1 = imcrop3(T1_SA,win);
+%     Joint_mask = imcrop3(Joint_mask,win);
+
+    vel1 = max(vel,size(T2_SA,[1:2])); 
+    JointT2 = insertMatrix(zeros(vel1(1),vel1(2),length(zz)), T2_SA);
+    JointT1 = insertMatrix(zeros(vel1(1),vel1(2),length(zz)), T1_SA);
+    Joint_mask = insertMatrix(zeros(vel1(1),vel1(2),length(zz)), Joint_mask);
+
+    %% saving
+    niftiwrite(Joint_mask ,[path_save_file '\'  '\Joint_T1_gt.nii'],'Compressed',true); 
+    niftiwrite(Joint_mask ,[path_save_file '\'  '\Joint_T2_gt.nii'],'Compressed',true); 
+
+    niftiwrite(JointT1 ,[path_save_file '\'  '\Joint_T1.nii'],'Compressed',true); 
+    niftiwrite(JointT2 ,[path_save_file '\'  '\Joint_T2.nii'],'Compressed',true); 
 
 %     vel(i,:) = size(T2);
 % %     catch error
